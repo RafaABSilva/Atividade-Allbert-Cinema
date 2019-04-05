6f80b29ad672e00c64cf301bf17f86b3f3626733
JoaoPauloCunha <jcpfcunha@gmail.com>
2019-04-04 22:03:17 -0300

    Create Filmes.cs
    criação dos filmes

---
+++ Atividade-Allbert-Cinema/Models/Filmes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Atividade_Allbert_Cinema.Models
+{
+    public class Filmes
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(120)]
+        public string Nome { get; set; }
+        [Required]
+        public int Duracao { get; set; }
+        [Required]
+        public int DataLancamento { get; set; }
+        [Required]
+        public virtual Categorias Categoria { get; set; }
+
+    }
+}
