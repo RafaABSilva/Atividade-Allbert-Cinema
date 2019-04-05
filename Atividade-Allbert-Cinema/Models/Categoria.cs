d32b11285057b74929b345a176e3432018bbb207
Rafael Alcântara <ra.fa.alcantara@hotmail.com>
2019-04-04 21:54:52 -0300

    Criação das Classes: Sala e Categoria

---
+++ Atividade-Allbert-Cinema/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Atividade_Allbert_Cinema.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(240)]
+        public string Descricao { get; set; }
+    }
+}
---
+++ Atividade-Allbert-Cinema/Models/Sala.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Atividade_Allbert_Cinema.Models
+{
+    public class Sala
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(120)]
+        public string Nome { get; set; }
+        [Required]
+        public int Capacidade { get; set; }
+        [Required]
+        public float TamanhoDaTela { get; set; }
+    }
+}
